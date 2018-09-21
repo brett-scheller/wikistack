a9fa234a9f7c481e98a47b93d20f125b211e9316
Brett Scheller <brett.green.scheller@gmail.com>
2018-09-21 11:10:05 -0500

    starting schemas

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const morgan = require("morgan");
 const express = require("express");
+const layout = require("./views/layout.js")
+const { db } = require('./models');
+
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
 
 const app = express();
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World</h1>");
+  res.send(layout("<h1>Hello World</h1>"));
 });
 
 const PORT = 1337;
